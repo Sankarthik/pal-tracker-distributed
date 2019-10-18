badd2ed8c1f54eed2e4da80202dcc748cdd46cf7
SankarG <sankaranarayanan.giridharan@rabobank.nl>
2019-10-18 16:03:49 +0200

    Adding Circuit Breaker for fallback mechanism

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,35 @@
 package io.pivotal.pal.tracker.allocations;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
+    private final Map<Long, ProjectInfo> projectInfoCache = new ConcurrentHashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations= restOperations;
+        this.restOperations = restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projectInfo = restOperations.getForObject(
+            registrationServerEndpoint + "/projects/" + projectId,
+            ProjectInfo.class
+        );
+        projectInfoCache.put(projectId, projectInfo);
+
+        return projectInfo;
     }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return projectInfoCache.get(projectId);
+    }
+
 }
